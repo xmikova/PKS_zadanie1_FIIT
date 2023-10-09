5dcf90f5ba0df178b055794ec3f43e9ff597a04e
[Petra Mikova] <61084407+PetraMikova@users.noreply.github.com>
2023-10-09 13:32:46 +0200

    part 2 done no pretty formatting

--- main.py
+++ main.py
@@ -7,18 +7,25 @@ from ruamel.yaml.scalarstring import LiteralScalarString
 
 
 class Packet:
-    def __init__(self, frame_number, frame_length, media_length, frame_type,
-                 destination_mac_address, source_mac_address, hex_dump,
-                 frame_pid=None, frame_sap=None):
+    def __init__(self, frame_number, len_frame_pcap, len_frame_medium, frame_type,
+                 src_mac, dst_mac, hexa_frame,
+                 pid=None, sap=None, ether_type=None, src_ip=None, dst_ip=None, protocol=None, src_port=None, dst_port=None, app_protocol=None):
         self.frame_number = frame_number
-        self.frame_length = frame_length
-        self.media_length = media_length
+        self.len_frame_pcap = len_frame_pcap
+        self.len_frame_medium = len_frame_medium
         self.frame_type = frame_type
-        self.destination_mac_address = destination_mac_address
-        self.source_mac_address = source_mac_address
-        self.frame_pid = frame_pid
-        self.frame_sap = frame_sap
-        self.hex_dump = hex_dump
+        self.src_mac = src_mac
+        self.dst_mac = dst_mac
+        self.ether_type = ether_type
+        self.src_ip = src_ip
+        self.dst_ip = dst_ip
+        self.protocol = protocol
+        self.src_port = src_port
+        self.dst_port = dst_port
+        self.app_protocol = app_protocol
+        self.pid = pid
+        self.sap = sap
+        self.hexa_frame = hexa_frame
 
 
 # Funkcia pre definovanie dĺžky rámca
@@ -35,31 +42,31 @@ def frame_length_medium(packet):
 def print_hex_dump(packet):
     hex_string = bytes(packet).hex()
 
-    formatted_string = ''
+    hex_dump_formatted = ''
 
     for i in range(0, len(hex_string), 2):
-        formatted_string += hex_string[i:i + 2] + ' '
+        hex_dump_formatted += hex_string[i:i + 2].upper() + ' '
 
         if (i + 2) % 32 == 0:
-            formatted_string += '\n'
+            hex_dump_formatted = hex_dump_formatted.rstrip()
+            hex_dump_formatted += '\n'
 
-    if len(hex_string) % 16 == 0:
-        return formatted_string
-    else:
-        return formatted_string + "\n"
+    hex_dump_formatted = hex_dump_formatted.rstrip()
+
+    return hex_dump_formatted + '\n'
 
 
 # Funkcia pre správny formát výpisu MAC adries
 def mac_address_format(mac_address):
     string_mac = str(mac_address)
-    upper_str = string_mac.upper()
+    string_mac_capitals = string_mac.upper()
 
-    formatted_mac = ''
+    mac_formatted = ''
 
-    for i in range(0, len(upper_str), 2):
-        formatted_mac += upper_str[i:i + 2] + ':'
+    for i in range(0, len(string_mac_capitals), 2):
+        mac_formatted += string_mac_capitals[i:i + 2] + ':'
 
-    return formatted_mac[:-1]
+    return mac_formatted[:-1]
 
 
 # Funkcia pre získanie typu rámca z bytov v packete
@@ -81,13 +88,20 @@ def frame_type(data_in_bytes):
             frame_type_string = "IEEE 802.3 LLC"
     return frame_type_string
 
+def hex_pairs_to_ip(hex_pairs):
+    ip_parts = [hex_pairs[i:i+2] for i in range(0, len(hex_pairs), 2)]  # Split into pairs
+    try:
+        decimal_parts = [str(int(hex_pair, 16)) for hex_pair in ip_parts]  # Convert to decimal
+        ip_address = ".".join(decimal_parts)  # Join parts with dots
+        return ip_address
+    except ValueError:
+        return "Invalid input"
 
 # Funkcia pre získanie údajov pre ďalšiu analýzu z externých súborov
 def get_data_from_file(file_string):
     data = {}
     with open(file_string, "r") as file:
         for line in file:
-            # Split the line at the first space character
             parts = line.strip().split(' ', 1)
             if len(parts) == 2:
                 hexa = parts[0]
@@ -103,6 +117,7 @@ packets = rdpcap(pcap_filename)
 frame_number = 1
 packet_frames = []
 
+
 for packet in packets:
     data_in_bytes = bytes(packet)
     ISL_header_check_hex = data_in_bytes[0:5].hex()
@@ -112,31 +127,63 @@ for packet in packets:
 
     frame_type_result = frame_type(data_in_bytes)
 
-    frame = Packet(
-        frame_number=frame_number,
-        frame_length=len(packet),
-        media_length=frame_length_medium(packet),
-        frame_type=frame_type_result,
-        destination_mac_address=mac_address_format(data_in_bytes[0:6].hex()),
-        source_mac_address=mac_address_format(data_in_bytes[6:12].hex()),
-        hex_dump=LiteralScalarString(print_hex_dump(packet))
-    )
+    frame = Packet(frame_number=frame_number, len_frame_pcap=len(packet), len_frame_medium=frame_length_medium(packet),
+                   frame_type=frame_type_result, src_mac=mac_address_format(data_in_bytes[6:12].hex()),
+                   dst_mac=mac_address_format(data_in_bytes[0:6].hex()),
+                   hexa_frame=LiteralScalarString(print_hex_dump(packet)))
+
+    if frame_type_result == "ETHERNET II":
+        ethertypes = get_data_from_file("Protocols/ETHERTYPE.txt")
+        frame_part3_hex = data_in_bytes[12:14].hex().upper()
+        frame_part3_string = "0x" + frame_part3_hex
+        ethertype_name = ethertypes.get(frame_part3_string, "Unknown type")
+        frame.ether_type = ethertype_name
+        if ethertype_name not in ["LLDP", "IPv6", "Unknown type"]:
+            source_ip = data_in_bytes[26:30].hex()
+            frame.src_ip = hex_pairs_to_ip(source_ip)
+            dest_ip = data_in_bytes[30:34].hex()
+            frame.dst_ip = hex_pairs_to_ip(dest_ip)
+        if ethertype_name == "IPv4":
+            protocols = get_data_from_file("Protocols/IP_PROTOCOLS.txt")
+            protocol = data_in_bytes[23:24].hex()
+            protocol_decimal = str(int(protocol, 16))
+            protocol_name = protocols.get(protocol_decimal, "Unknown type")
+            frame.protocol = protocol_name
+            if protocol_name == "TCP" or protocol_name == "UDP":
+                source_port = data_in_bytes[34:36].hex()
+                source_port_decimal = int(source_port, 16)
+                frame.src_port = source_port_decimal
+                dest_port = data_in_bytes[36:38].hex()
+                dest_port_decimal = int(dest_port, 16)
+                frame.dst_port = dest_port_decimal
+                if protocol_name == "TCP":
+                    tcp_server_ports = get_data_from_file("Protocols/TCP.txt")
+                    if str(source_port_decimal) in tcp_server_ports:
+                        frame.app_protocol = tcp_server_ports.get(str(source_port_decimal))
+                    elif frame.app_protocol is None and str(dest_port_decimal) in tcp_server_ports:
+                        frame.app_protocol = tcp_server_ports.get(str(dest_port_decimal))
+                elif protocol_name == "UDP":
+                    udp_server_ports = get_data_from_file("Protocols/UDP.txt")
+                    if str(source_port_decimal) in udp_server_ports:
+                        frame.app_protocol = udp_server_ports.get(str(source_port_decimal))
+                    elif frame.app_protocol is None and str(dest_port_decimal) in udp_server_ports:
+                        frame.app_protocol = udp_server_ports.get(str(dest_port_decimal))
 
     if frame_type_result == "IEEE 802.3 LLC & SNAP":
-        pids = get_data_from_file("PID.txt")
-        frame_pid_hex = data_in_bytes[20:22].hex()
+        pids = get_data_from_file("Protocols/PID.txt")
+        frame_pid_hex = data_in_bytes[20:22].hex().upper()
         frame_pid_string = "0x" + frame_pid_hex
         pid_name = pids.get(frame_pid_string, "Unknown type")
-        frame.frame_pid = pid_name
+        frame.pid = pid_name
 
     if frame_type_result == "IEEE 802.3 LLC":
-        saps = get_data_from_file("LLC.txt")
-        frame_sap_hex = data_in_bytes[14:15].hex()
+        saps = get_data_from_file("Protocols/LLC.txt")
+        frame_sap_hex = data_in_bytes[14:15].hex().upper()
         frame_sap_string = "0x" + frame_sap_hex
         sap_name = saps.get(frame_sap_string, "Unknown type")
-        frame.frame_sap = sap_name
+        frame.sap = sap_name
 
-    # Vyfiltrujeme atribúty s None value pre výpis do yamlu
+    # Vyfiltrujeme preč atribúty s None value pre správny výpis do yamlu
     frame_dict = {k: v for k, v in frame.__dict__.items() if v is not None}
 
     packet_frames.append(frame_dict)
